37b74d3e08ef572fa27670640a238bd698b0acdd
leetaejun <leetaejun147@gmail.com>
2016-03-10 22:21:24 +0900

    CommonCode 시드에 상세카테고리 추가

--- db/seeds/01_common_code.rb
+++ db/seeds/01_common_code.rb
@@ -48,3 +48,808 @@ CommonCode.create(
   detail_code: 3,
   detail_name: '염색'
 )
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY',
+  detail_code: 4,
+  detail_name: '클리닉'
+)
+
+# 상세 - 여자 롱 컷
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_CUT',
+  detail_code: 1,
+  detail_name: '레이어드컷'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_CUT',
+  detail_code: 2,
+  detail_name: '뱅헤어'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_CUT',
+  detail_code: 3,
+  detail_name: '비대칭컷'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_CUT',
+  detail_code: 4,
+  detail_name: '샤기컷'
+)
+
+# 상세 - 여자 롱 펌
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 1,
+  detail_name: 'C컬펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 2,
+  detail_name: 'S컬펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 3,
+  detail_name: '굵은롤펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 4,
+  detail_name: '글램펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 5,
+  detail_name: '내츄럴펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 6,
+  detail_name: '디지털펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 7,
+  detail_name: '러블리펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 8,
+  detail_name: '글램펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 9,
+  detail_name: '루즈펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 10,
+  detail_name: '물결펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 11,
+  detail_name: '바디펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 12,
+  detail_name: '발롱펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 13,
+  detail_name: '텍스쳐펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 14,
+  detail_name: '볼륨매직'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 15,
+  detail_name: '볼륨펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 16,
+  detail_name: '셋팅펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 17,
+  detail_name: '아이롱펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_PERM',
+  detail_code: 18,
+  detail_name: '웨이브펌'
+)
+
+# 상세 - 여자 롱 염색
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 1,
+  detail_name: '다크브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 2,
+  detail_name: '레드브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 3,
+  detail_name: '레드와인'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 4,
+  detail_name: '밀크브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 5,
+  detail_name: '보라색'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 6,
+  detail_name: '브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 7,
+  detail_name: '블루블랙'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 8,
+  detail_name: '애쉬그레이'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 9,
+  detail_name: '애쉬브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 10,
+  detail_name: '애쉬블론드'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 11,
+  detail_name: '카키브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 12,
+  detail_name: '투톤'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_LONG_DYE',
+  detail_code: 13,
+  detail_name: '파스텔'
+)
+
+# 상세 - 여자 미디움 컷
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_CUT',
+  detail_code: 1,
+  detail_name: '뱅헤어'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_CUT',
+  detail_code: 2,
+  detail_name: '비대칭컷'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_CUT',
+  detail_code: 3,
+  detail_name: '샤기컷'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_CUT',
+  detail_code: 4,
+  detail_name: '보브컷'
+)
+
+# 상세 - 여자 미디움 펌
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 1,
+  detail_name: 'C컬펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 2,
+  detail_name: 'S컬펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 3,
+  detail_name: '굵은롤펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 4,
+  detail_name: '글램펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 5,
+  detail_name: '내츄럴펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 6,
+  detail_name: '디지털펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 7,
+  detail_name: '러블리펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 8,
+  detail_name: '루즈펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 9,
+  detail_name: '물결펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 10,
+  detail_name: '바디펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 11,
+  detail_name: '발롱펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 12,
+  detail_name: '텍스쳐펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 13,
+  detail_name: '볼륨매직'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 14,
+  detail_name: '볼륨펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 15,
+  detail_name: '셋팅펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 16,
+  detail_name: '아이롱펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_PERM',
+  detail_code: 17,
+  detail_name: '웨이브펌'
+)
+
+# 상세 - 여자 미디움 염색
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 1,
+  detail_name: '다크브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 2,
+  detail_name: '레드브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 3,
+  detail_name: '레드와인'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 4,
+  detail_name: '밀크브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 5,
+  detail_name: '보라색'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 6,
+  detail_name: '브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 7,
+  detail_name: '블루블랙'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 8,
+  detail_name: '애쉬그레이'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 9,
+  detail_name: '애쉬브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 10,
+  detail_name: '애쉬블론드'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 11,
+  detail_name: '카키브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 12,
+  detail_name: '투톤'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_MEDIUM_DYE',
+  detail_code: 13,
+  detail_name: '파스텔'
+)
+
+# 상세 - 여자 단발 컷
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_CUT',
+  detail_code: 1,
+  detail_name: '레이어드컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_CUT',
+  detail_code: 2,
+  detail_name: '뱅헤어' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_CUT',
+  detail_code: 3,
+  detail_name: '비대칭컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_CUT',
+  detail_code: 4,
+  detail_name: '샤기컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_CUT',
+  detail_code: 5,
+  detail_name: '보브컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_CUT',
+  detail_code: 6,
+  detail_name: '머쉬룸컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_CUT',
+  detail_code: 7,
+  detail_name: '투블럭컷' 
+)
+
+# 상세 - 여자 단발 펌
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 1,
+  detail_name: 'C컬펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 2,
+  detail_name: 'S컬펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 3,
+  detail_name: '굵은롤펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 4,
+  detail_name: '글램펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 5,
+  detail_name: '내츄럴펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 6,
+  detail_name: '디지털펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 7,
+  detail_name: '러블리펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 8,
+  detail_name: '루즈펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 9,
+  detail_name: '물결펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 10,
+  detail_name: '바디펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 11,
+  detail_name: '발롱펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 12,
+  detail_name: '베이비펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 13,
+  detail_name: '텍스쳐펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 14,
+  detail_name: '볼륨매직'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 15,
+  detail_name: '볼륨펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 16,
+  detail_name: '셋팅펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 17,
+  detail_name: '아이롱펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_PERM',
+  detail_code: 18,
+  detail_name: '웨이브펌'
+)
+
+# 상세 - 여자 단발 염색
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 1,
+  detail_name: '다크브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 2,
+  detail_name: '레드브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 3,
+  detail_name: '레드와인'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 4,
+  detail_name: '밀크브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 5,
+  detail_name: '보라색'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 6,
+  detail_name: '브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 7,
+  detail_name: '블루블랙'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 8,
+  detail_name: '애쉬그레이'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 9,
+  detail_name: '애쉬브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 10,
+  detail_name: '애쉬블론드'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 11,
+  detail_name: '카키브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 12,
+  detail_name: '투톤'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_BOB_DYE',
+  detail_code: 13,
+  detail_name: '파스텔'
+)
+
+# 상세 - 여자 숏 컷
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_CUT',
+  detail_code: 1,
+  detail_name: '레이어드컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_CUT',
+  detail_code: 2,
+  detail_name: '뱅헤어' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_CUT',
+  detail_code: 3,
+  detail_name: '비대칭컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_CUT',
+  detail_code: 4,
+  detail_name: '샤기컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_CUT',
+  detail_code: 5,
+  detail_name: '보브컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_CUT',
+  detail_code: 6,
+  detail_name: '머쉬룸컷' 
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_CUT',
+  detail_code: 7,
+  detail_name: '투블럭컷' 
+)
+
+# 상세 - 여자 숏 펌
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 1,
+  detail_name: 'C컬펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 2,
+  detail_name: 'S컬펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 3,
+  detail_name: '굵은롤펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 4,
+  detail_name: '글램펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 5,
+  detail_name: '내츄럴펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 6,
+  detail_name: '디지털펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 7,
+  detail_name: '러블리펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 8,
+  detail_name: '루즈펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 9,
+  detail_name: '물결펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 10,
+  detail_name: '바디펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 11,
+  detail_name: '발롱펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 12,
+  detail_name: '베이비펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 13,
+  detail_name: '텍스쳐펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 14,
+  detail_name: '볼륨매직'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 15,
+  detail_name: '볼륨펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 16,
+  detail_name: '셋팅펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 17,
+  detail_name: '아이롱펌'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_PERM',
+  detail_code: 18,
+  detail_name: '웨이브펌'
+)
+
+# 상세 - 여자 숏 염색
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 1,
+  detail_name: '다크브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 2,
+  detail_name: '레드브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 3,
+  detail_name: '레드와인'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 4,
+  detail_name: '밀크브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 5,
+  detail_name: '보라색'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 6,
+  detail_name: '브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 7,
+  detail_name: '블루블랙'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 8,
+  detail_name: '애쉬그레이'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 9,
+  detail_name: '애쉬브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 10,
+  detail_name: '애쉬블론드'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 11,
+  detail_name: '카키브라운'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 12,
+  detail_name: '투톤'
+)
+CommonCode.create(
+  main_code: 'HAIR_CATEGORY_WOMAN_SHORT_DYE',
+  detail_code: 13,
+  detail_name: '파스텔'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
